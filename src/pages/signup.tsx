098387e814d9b7521ae6a8b82b207b3d80d22329
Victory Deo <victory23tshimbadi@gmail.com>
2024-01-14 16:17:02 +0100

    Add loader to indicate form submission

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { FcGoogle } from "@react-icons/all-files/fc/FcGoogle";
+import React, { useState } from 'react';
+import { FcGoogle } from '@react-icons/all-files/fc/FcGoogle';
+import { ColorRing } from 'react-loader-spinner';
 
 const Signup = () => {
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+        setIsLoading(true);
+	};
+
 	return (
 		<main className='bg-white w-full h-screen md:h-max flex flex-col justify-center items-center'>
-			<form className='p-4 w-11/12 md:w-[400px] flex flex-col justify-center gap-y-4'>
+			<form onSubmit={handleSubmit} className='p-4 w-11/12 md:w-[400px] flex flex-col justify-center gap-y-4'>
 				<div className='flex flex-col gap-2'>
 					<h3 className='text-sm text-shark-950 text-normal font-normal'>
 						Bienvenu(e)
@@ -25,9 +33,10 @@ const Signup = () => {
 						id='email'
 						autoComplete='on'
 						placeholder='johndoe@gmail.com'
+						required
 						className='p-1.5 outline outline-1 outline-shark-400 rounded-md text-shark-950 focus:outline-shark-950'
 					/>
-                    <p className='text-red-500 text-sm'>Field Required</p>
+					<p className='text-red-500 text-sm'>Field Required</p>
 				</div>
 				<div className='flex flex-col gap-2'>
 					<label
@@ -41,9 +50,10 @@ const Signup = () => {
 						id='name'
 						autoComplete='on'
 						placeholder='Doe'
+						required
 						className='p-1.5 outline outline-1 outline-shark-400 rounded-md text-shark-950 focus:outline-shark-950'
 					/>
-                    <p className='text-red-500 text-sm'>Field Required</p>
+					<p className='text-red-500 text-sm'>Field Required</p>
 				</div>
 				<div className='flex flex-col gap-2'>
 					<label
@@ -57,9 +67,10 @@ const Signup = () => {
 						id='firstname'
 						autoComplete='on'
 						placeholder='John'
+						required
 						className='p-1.5 outline outline-1 outline-shark-400 rounded-md text-shark-950 focus:outline-shark-950'
 					/>
-                    <p className='text-red-500 text-sm'>Field Required</p>
+					<p className='text-red-500 text-sm'>Field Required</p>
 				</div>
 				<div className='flex flex-col gap-2'>
 					<label
@@ -73,9 +84,10 @@ const Signup = () => {
 						id='password'
 						autoComplete='on'
 						placeholder='********'
+						required
 						className='p-1.5 outline outline-1 outline-shark-400 rounded-md text-shark-950 focus:outline-shark-950'
 					/>
-                    <p className='text-red-500 text-sm'>Field Required</p>
+					<p className='text-red-500 text-sm'>Field Required</p>
 				</div>
 				<div className='flex flex-col gap-2'>
 					<label
@@ -89,20 +101,39 @@ const Signup = () => {
 						id='confirmPassword'
 						autoComplete='on'
 						placeholder='********'
+						required
 						className='p-1.5 outline outline-1 outline-shark-400 rounded-md text-shark-950 focus:outline-shark-950'
 					/>
-                    <p className='text-red-500 text-sm'>Field Required</p>
+					<p className='text-red-500 text-sm'>Field Required</p>
 				</div>
 				<div className='flex flex-col justify-center items-center gap-2.5'>
 					<button
 						type='submit'
-						className='w-full py-2 text-center text-white text-base font-medium bg-blue-500 rounded-lg hover:bg-blue-700'>
-						{"S'inscrire"}
+						disabled={isLoading ? true : false}
+						className={
+							isLoading
+								? 'flex justify-center items-center w-full py-2 text-center text-white text-base font-medium bg-blue-100 rounded-lg'
+								: 'w-full py-2 text-center text-white text-base font-medium bg-blue-500 rounded-lg hover:bg-blue-700'
+						}>
+						{isLoading ? (
+							<ColorRing
+								visible={true}
+								height='24'
+								width='24'
+								ariaLabel='color-ring-loading'
+								wrapperStyle={{}}
+								wrapperClass='color-ring-wrapper'
+								colors={['#1d4ed8', '#1d4ed8', '#1d4ed8', '#1d4ed8', '#1d4ed8']}
+							/>
+						) : (
+							"S'inscrire"
+						)}
 					</button>
-                    <button
+					<button
 						type='button'
 						className='w-full flex justify-center items-center gap-2 py-2 text-center text-shark-950 text-base font-medium rounded-lg border border-shark-500 hover:border-shark-600'>
-						<FcGoogle size={24}/>{'Continuer avec Google'}
+						<FcGoogle size={24} />
+						{'Continuer avec Google'}
 					</button>
 				</div>
 			</form>
